dbdd005d210e3b364d48af2e67e553d7979fbfb0
Ethan Glover <hello@ethang.email>
2024-07-22 09:06:19 -0500

    Update Commit

--- build.mjs
+++ build.mjs
@@ -2,6 +2,6 @@ import { projectBuilder } from "@ethang/project-builder/project-builder.js";
 
 await projectBuilder("sterett-react", "main", {
 	preVersionBumpScripts: ["UPDATE"],
-	postVersionBumpScripts: ["DEDUPE", "LINT", "BUILD"],
+	postVersionBumpScripts: ["DEDUPE", "LINT"],
 	isLibrary: false,
 });
