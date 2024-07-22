4936358c2b19db624192581727ead926ebe176d0
Ethan Glover <hello@ethang.email>
2024-07-22 09:05:18 -0500

    Update Commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,7 @@
 import react from "@vitejs/plugin-react";
-import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["lodash", "moment", "react-big-calendar"],
-        },
-      },
-    },
-  },
-  plugins: [
-    react(),
-    visualizer({
-      brotliSize: true,
-      gzipSize: true,
-    }),
-  ],
+  plugins: [react()],
 });
