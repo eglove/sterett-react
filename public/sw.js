25aecea756917bf884bedc21075077bd7186cf18
Ethan Glover <hello@ethang.email>
2024-07-23 12:47:44 -0500

    clean cache

--- public/sw.js
+++ public/sw.js
@@ -2,11 +2,15 @@
 // @ts-nocheck
 /* eslint-disable unicorn/no-abusive-eslint-disable */
 /* eslint-disable */
+const VERSION = 1
+const currentCacheName = `http-cache-v${VERSION}`
+
 self.addEventListener('install', async () => {
 	return self.skipWaiting()
 })
 
 self.addEventListener('activate', async () => {
+	cleanCache()
 	return self.clients.claim()
 })
 
@@ -18,7 +22,7 @@ self.addEventListener('fetch', async (event) => {
 		return
 	}
 
-	const cache = await caches.open('http-cache')
+	const cache = await caches.open(currentCacheName)
 
 	const cachedResponse = await cache.match(event.request)
 
@@ -35,9 +39,18 @@ self.addEventListener('fetch', async (event) => {
 	}
 })
 
+async function cleanCache () {
+	const keys = await caches.keys()
+	for (const key of keys) {
+		if (key !== currentCacheName) {
+			caches.delete(key)
+		}
+	}
+}
+
 async function fetchAndUpdateCache (cache, request) {
 	const response = await fetch(request).catch(error => {
-		console.error(error);
+		console.error(error)
 	})
 
 	if (response.ok) {
