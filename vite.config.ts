e7b1b5dbef189a25dcc2827731d880e485651700
Ethan Glover <hello@ethang.email>
2024-07-23 15:07:26 -0500

    build worker with workbox

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      workbox: {
+        runtimeCaching: [
+          {
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+              cacheName: "http-cache",
+              expiration: {
+                maxAgeSeconds: 60,
+              },
+            },
+            urlPattern: ({ url }) => {
+              const { protocol } = new URL(url);
+
+              return "http:" === protocol || "https:" === protocol;
+            },
+          },
+        ],
+      },
+    }),
+  ],
 });
