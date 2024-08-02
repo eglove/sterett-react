1ce29aa1c62f4e2001db1816d531dbdf91c8c351
Ethan Glover <hello@ethang.email>
2024-08-01 19:42:09 -0500

    add to calendar button

--- src/components/event.tsx
+++ src/components/event.tsx
@@ -1,12 +1,20 @@
-import { Dispatch, SetStateAction, useEffect } from "react";
+import type { Dispatch, SetStateAction } from "react";
 
 import { CalendarDaysIcon } from "@heroicons/react/24/solid";
+import { Button } from "@nextui-org/button";
 import { Card, CardBody, CardHeader } from "@nextui-org/card";
 import { Divider } from "@nextui-org/divider";
+import {
+  CardFooter,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from "@nextui-org/react";
+import { google, ics, office365, outlook, yahoo } from "calendar-link";
 import isNil from "lodash/isNil";
 import { DateTime } from "luxon";
-import { useState } from "react";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 import type { CalendarEventReturn } from "../sanity/queries/get-news-and-events.ts";
@@ -39,6 +47,12 @@ function dateIsInRange(start: string, end: string) {
   return now >= startDate && now <= endDate;
 }
 
+function openNewTab(url: string) {
+  if ("undefined" !== typeof window) {
+    window.open(url, "_blank")?.focus();
+  }
+}
+
 export function Event({
   colors,
   data,
@@ -51,28 +65,34 @@ export function Event({
   const [isDateShowing, setIsDateShowing] = useState(false);
 
   const backgroundColor = isNil(colors?.eventBackground)
-      ? "bg-sky-200"
-      : `bg-${colors.eventBackground}`;
+    ? "bg-sky-200"
+    : `bg-${colors.eventBackground}`;
   const textColor = isNil(colors?.eventText)
-      ? "text-foreground"
-      : colors.eventText;
+    ? "text-foreground"
+    : colors.eventText;
+
+  const calendarData = {
+    description: "",
+    end: data.endsAt,
+    start: data.startsAt,
+    title: data.title,
+  };
 
   useEffect(() => {
-    if (!usedDates?.has(relativeDate) && !isInRange) {
-      setIsDateShowing(true);
-      setUsedDates?.((previousState) => {
-        return new Set(previousState).add(relativeDate);
-      });
-    }
+      if (!usedDates?.has(relativeDate) && !isInRange) {
+          setIsDateShowing(true);
+          setUsedDates?.((previousState) => {
+              return new Set(previousState).add(relativeDate);
+          });
+      }
 
-    if (!usedDates?.has(happeningNow) && isInRange) {
-      setIsDateShowing(true);
-      setUsedDates?.((previousState) => {
-        return new Set(previousState).add(happeningNow);
-      });
-    }
+      if (!usedDates?.has(happeningNow) && isInRange) {
+          setIsDateShowing(true);
+          setUsedDates?.((previousState) => {
+              return new Set(previousState).add(happeningNow);
+          });
+      }
   }, [usedDates, happeningNow, relativeDate, isInRange]);
-
 
   return (
     <Fragment key={data._id}>
@@ -112,8 +132,60 @@ export function Event({
             <CardBody>
               <SanityContent styleNames={textColor} value={data.description} />
             </CardBody>
+            <Divider />
           </>
         )}
+        <CardFooter className="flex gap-2 flex-wrap">
+          <Dropdown>
+            <DropdownTrigger>
+              <Button className="bg-sky-600 text-white" size="sm">
+                Add To Calendar
+              </Button>
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Calendar Options" selectionMode="single">
+              <DropdownItem
+                onPress={() => {
+                  openNewTab(google(calendarData));
+                }}
+                key="google"
+              >
+                Google
+              </DropdownItem>
+              <DropdownItem
+                onPress={() => {
+                  openNewTab(outlook(calendarData));
+                }}
+                key="outlook"
+              >
+                Outlook
+              </DropdownItem>
+              <DropdownItem
+                onPress={() => {
+                  openNewTab(office365(calendarData));
+                }}
+                key="office365"
+              >
+                Office 365
+              </DropdownItem>
+              <DropdownItem
+                onPress={() => {
+                  openNewTab(yahoo(calendarData));
+                }}
+                key="yahoo"
+              >
+                Yahoo
+              </DropdownItem>
+              <DropdownItem
+                onPress={() => {
+                  openNewTab(ics(calendarData));
+                }}
+                key="ics"
+              >
+                ICS
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+        </CardFooter>
       </Card>
     </Fragment>
   );
