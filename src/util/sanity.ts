5a9dd280e515c39dafd5cbb04607a494f79c1f15
Ethan Glover <hello@ethang.email>
2024-08-02 08:29:57 -0500

    lint fix

--- src/util/sanity.ts
+++ src/util/sanity.ts
@@ -7,12 +7,13 @@ export function toPlainText(blocks: PortableTextBlock[] = []) {
   const result = attempt(() => {
     return blocks
       .map((block) => {
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition,@typescript-eslint/strict-boolean-expressions
         if ("block" !== block._type || !block.children) {
           return "";
         }
         return block.children
           .map((child) => {
-            return child.text;
+            return child.text as string;
           })
           .join("");
       })
