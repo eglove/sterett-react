5864fe97b801136d8d062ddbeab6d3aba436e335
Ethan Glover <hello@ethang.email>
2024-08-03 23:21:38 -0500

    use meta config

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import config from "@ethang/eslint-config/eslint.config.js";
+import configReact from "@ethang/eslint-config/config.react.js";
 import tseslint from "typescript-eslint";
-import configReact from "@ethang/eslint-config-react";
 
 export default tseslint.config(...config, ...configReact, {
   ignores: ["dist/"], // Your ignores directories
