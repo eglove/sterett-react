5961fcbd1a9bf19df6d582e28d50d5912019f438
Ethan <hello@ethang.email>
2024-08-08 20:09:35 -0500

    Add BUILD script to projectBuilder in build.mjs
    This change includes the BUILD script in the projectBuilder configuration for the sterett-react project. It ensures the build script is executed along with update, dedupe, and lint scripts. The modification aims to streamline the build process.

--- build.mjs
+++ build.mjs
@@ -1,6 +1,6 @@
 import { projectBuilder } from "@ethang/project-builder/project-builder.js";
 
 await projectBuilder("sterett-react", "main", {
-	scripts: ["UPDATE", "DEDUPE", "LINT"],
+	scripts: ["UPDATE", "DEDUPE", "LINT", "BUILD"],
 	isLibrary: false,
 });
