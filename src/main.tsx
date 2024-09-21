077269e638b9a6cf283784e7e242e75525013d0a
Ethan <hello@ethang.email>
2024-09-21 14:51:34 -0500

    update

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { router } from "./routes/route-tree.ts";
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion,ethang/handle-native-error
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.querySelector("#root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
