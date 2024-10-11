9ee6887d920b6cfe6453224e6eadc79064b468a6
Ethan <hello@ethang.email>
2024-10-11 09:08:06 -0500

    dedupe

--- build.mjs
+++ build.mjs
@@ -2,5 +2,5 @@ import { projectBuilder } from "@ethang/project-builder/project-builder.js";
 
 await projectBuilder("sterett-react", "main", {
   isLibrary: false,
-  scripts: ["pnpm up -i --latest", "pnpm lint", "pnpm build"],
+  scripts: ["pnpm up -i --latest", "pnpm dedupe", "pnpm lint", "pnpm build"],
 });
