9f1958d66baf7ffce64896b8eb7f9d9006db17a5
Ethan <hello@ethang.email>
2024-10-16 08:23:32 -0500

    rmove ts ignore

--- src/routes/page.id.tsx
+++ src/routes/page.id.tsx
@@ -41,7 +41,6 @@ export const pageIdRoute = createRoute({
     return rootRoute;
   },
   async loader(context) {
-    // @ts-expect-error router type issues
     return queryClient.ensureQueryData(getPageQueryOptions(context.params.id));
   },
   path: "/page/$id",
