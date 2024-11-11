ae3aeaeac159dc5e8cad484bdcafcadfa21070d6
Ethan <hello@ethang.email>
2024-11-10 20:53:23 -0600

    reinstall

--- build.mjs
+++ build.mjs
@@ -2,5 +2,5 @@ import { projectBuilder } from "@ethang/project-builder/project-builder.js";
 
 await projectBuilder("sterett-react", "main", {
   isLibrary: false,
-  scripts: ["trash pnpm-lock.yaml", "pnpm up -i --latest", "pnpm dedupe", "pnpm lint", "pnpm build"],
+  scripts: ["trash pnpm-lock.yaml", "pnpm i", "pnpm up -i --latest", "pnpm dedupe", "pnpm lint", "pnpm build"],
 });
